9780e5f81a8c23c5dc3fde1562e862b890637438
Administrator <Administrator@XQ-20150326QHNH>
2018-05-05 20:37:54 +0800

    修改后的提交

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloWorld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-     System.out.println("这是一个测试文件");
+        System.out.println("这是一个测试文件----修改后的");
 	}
 }
